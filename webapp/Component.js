8ca09b8c5da1d329bc4a1067177c418cc52f2ec8
Carlos Mendez <jose.mendez@people-media.com.mx>
2016-04-12 17:20:42 -0500

    Primer

---
+++ webapp/Component.js
@@ -0,0 +1,11 @@
+sap.ui.core.UIComponent.extend("pmedia.demo.cartera.Component", {
+	metadata: {
+		name: "Cartera",
+		version: "1.0.0",
+		includes: ["css/styles.css"],
+		dependencies: {
+			libs: ["sap.m"]
+		},
+		rootView: "pmedia.demo.cartera.view.App"
+	}
+});
